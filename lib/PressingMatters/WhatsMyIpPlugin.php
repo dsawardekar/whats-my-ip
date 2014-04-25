4f8050e3c1ee521a468cf035de5a75991abd3af8
Darshan Sawardekar <darshan@sawardekar.org>
2014-04-25 17:01:24 +0530

    Adds ScriptPlacer and main plugin file.

--- lib/PressingMatters/WhatsMyIpPlugin.php
+++ lib/PressingMatters/WhatsMyIpPlugin.php
@@ -4,17 +4,34 @@ namespace PressingMatters;
 
 use Encase\Container;
 use WordPress\TwigHelper;
+use PressingMatters\WhatsMyIpShortcode;
+use PressingMatters\ScriptPlacer;
 
 class WhatsMyIpPlugin {
 
   public $container;
 
-  function __construct() {
+  function __construct($file) {
     $this->container = new Container();
-    $this->container->factory('twig_helper', 'WordPress\\TwigHelper', true);
+    $this->container->object('baseDir', plugin_dir_path($file));
+    $this->container->singleton('twigHelper', 'WordPress\\TwigHelper');
+    $this->container->singleton('shortcode', 'PressingMatters\\WhatsMyIpShortcode');
+    $this->container->singleton('scriptPlacer', 'PressingMatters\\ScriptPlacer');
   }
 
-  function getTwigHelper() {
-    return $this->container->lookup('twig_helper');
+  function lookup($key) {
+    return $this->container->lookup($key);
+  }
+
+  function enable() {
+    $twigHelper = $this->lookup('twigHelper');
+    $twigHelper->setBaseDir($this->lookup('baseDir'));
+
+    add_shortcode('whatsmyip', array($this, 'doShortcode'));
+  }
+
+  function doShortcode($params) {
+    $this->lookup('scriptPlacer')->enable();
+    return $this->lookup('shortcode')->render($params);
   }
 }
--- test/PressingMatters/WhatsMyIpPluginTest.php
+++ test/PressingMatters/WhatsMyIpPluginTest.php
@@ -7,7 +7,8 @@ class WhatsMyIpPluginTest extends \PHPUnit_Framework_TestCase {
   public $plugin;
 
   function setUp() {
-    $this->plugin = new WhatsMyIpPlugin();
+    $file = getcwd() . '/whats-my-ip.php';
+    $this->plugin = new WhatsMyIpPlugin($file);
   }
 
   function test_it_has_a_container() {
@@ -15,9 +16,40 @@ class WhatsMyIpPluginTest extends \PHPUnit_Framework_TestCase {
     $this->assertInstanceOf('Encase\\Container', $container);
   }
 
+  function test_it_has_a_base_dir() {
+    $dir = $this->plugin->lookup('baseDir');
+    $this->assertStringStartsWith(getcwd(), $dir);
+  }
+
   function test_it_has_a_twig_helper() {
-    $helper = $this->plugin->getTwigHelper();
+    $helper = $this->plugin->lookup('twigHelper');
     $this->assertInstanceOf('WordPress\\TwigHelper', $helper);
+  }
+
+  function test_it_has_a_shortcode() {
+    $shortcode = $this->plugin->lookup('shortcode');
+    $this->assertInstanceOf('PressingMatters\\WhatsMyIpShortcode', $shortcode);
+  }
+
+  function test_it_has_a_script_placer() {
+    $placer = $this->plugin->lookup('scriptPlacer');
+    $this->assertInstanceOf('PressingMatters\\ScriptPlacer', $placer);
+  }
+
+  function test_it_can_render_shortcode() {
+    $this->plugin->enable();
+
+    $params = shortcode_parse_atts('');
+    $html = $this->plugin->doShortcode($params);
+
+    $matcher = array(
+      'tag' => 'p',
+      'attributes' => array('class' => 'ip')
+    );
+
+    $this->assertTag($matcher, $html);
+    $this->assertTrue(wp_script_is('whats-my-ip', 'enqueued'));
+    $this->assertTrue(wp_script_is('whats-my-ip-run', 'enqueued'));
   }
 
 }
