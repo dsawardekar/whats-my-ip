15f766055a8d40f1edcd0ead941d9721bb50340b
Darshan Sawardekar <darshan@sawardekar.org>
2014-05-16 18:02:26 +0530

    Upgrades to Arrow 0.7.0

--- whats-my-ip.php
+++ whats-my-ip.php
@@ -9,29 +9,16 @@ Plugin URI: http://wordpress.org/plugins/whats-my-ip
 License: GPLv2
 */
 
-require_once(__DIR__ . '/vendor/dsawardekar/wp-requirements/lib/Requirements.php');
+require_once(__DIR__ . '/vendor/dsawardekar/arrow/lib/Arrow/ArrowPluginLoader.php');
 
 function whats_my_ip_main() {
-  $requirements = new WP_Requirements();
+  $options = array(
+    'name'         => 'Whats My IP',
+    'plugin'       => 'WhatsMyIp\Plugin',
+    'arrowVersion' => '0.6.0'
+  );
 
-  if ($requirements->satisfied()) {
-    whats_my_ip_register();
-  } else {
-    $plugin = new WP_Faux_Plugin('Whats My IP', $requirements->getResults());
-    $plugin->activate(__FILE__);
-  }
-}
-
-function whats_my_ip_register() {
-  require_once(__DIR__ . '/vendor/dsawardekar/arrow/lib/Arrow/ArrowPluginLoader.php');
-
-  $loader = ArrowPluginLoader::getInstance();
-  $loader->register(__FILE__, '0.5.1', 'whats_my_ip_load');
-}
-
-function whats_my_ip_load() {
-  $plugin = \WhatsMyIp\Plugin::create(__FILE__);
-  $plugin->enable();
+  ArrowPluginLoader::load(__FILE__, $options);
 }
 
 whats_my_ip_main();
